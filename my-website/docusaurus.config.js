a599e7ce5a758339676a4b58985e54790c65f16c
wisroot-com <hisayoshi.furihata@wisroot.com>
2024-12-20 20:18:50 +0900

    update

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,13 +6,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://wisroot-com.github.io/',
+  baseUrl: '/xnew-doc',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wisroot-com', // Usually your GitHub org/user name.
+  projectName: 'xnew-doc', // Usually your repo name.
 
   presets: [
     [
